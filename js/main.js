89716d55a55afd0e050d0c041d21fe88f3ff946b
Midori Armstrong <midoriarmstrong@outlook.com>
2017-01-17 21:32:09 -0500

    Remove all tables and fix mobile bug

--- js/main.js
+++ js/main.js
@@ -17,20 +17,38 @@ $(document).ready(function() {
     $('main > a').fadeOut();
   });
   
+  // Ouputting error/success messages for contact form.
   $('section#mail form').submit(function(e) {  
     e.preventDefault();
+    var mail = $('section#mail > p');
     
     if ($('section#mail input[name="entry.731334852"]').val() != "" && $('section#mail textarea').val() != "") {
-      $('section#mail > p').removeAttr('class');
-      $('section#mail > p').text('Your message has been sent.').fadeIn();
-      $('section#mail > p').attr('class', 'success');
-      $('section#mail input').val('');
-      $('section#mail textarea').val('');      
+      mail.removeAttr('class');
+      mail.text('Your message has been sent.').fadeIn();
+      mail.attr('class', 'success');
+      $('section#mail label > input, textarea').val('');
+     
       this.submit();
     } else {
-      $('section#mail > p').text('Error: please fill in all required fields.').fadeIn();
-      $('section#mail > p').attr('class', 'error');
+      mail.text('Error: please fill in all required fields.').fadeIn();
+      mail.attr('class', 'error');
     } 
   });
+  
+  // Toggling main menu on mobile devices
+  $('header > nav > ul > li:last-of-type').click(function() {
+    var prev = $(this).prevAll();
+ 
+    // If menu items are hidden 
+    if (prev.css('display') == 'none') {
+      prev.slideDown(); // Slide down menu items
+
+    } else {
+      prev.slideUp(function() { // Slide up menu items
+        prev.removeAttr('style'); // Remove superfluous in-line style element to prevent issues when shifting to larger display size
+      });
+    }
+  });
+  
  });
 
